3642e5efe3a0c1aa16297f72bb409ca4972d7776
ioanmarcu <ioan.marcu@nttdata.ro>
2018-04-16 16:31:52 +0300

    Day 1, Ex 7 fix

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,1 +1,32 @@
-import {Injectable} from '@angular/core';import {CONTACT_DATA} from './data/contact-data';import {HttpClient, HttpClientModule, HttpParams} from '@angular/common/http';import {map} from 'rxjs/operators';import {Contact} from './models/contact';import {Observable} from 'rxjs/Observable';interface ContactResponse  { item: Contact; }interface ContactsResponse { items: Contact[]; }@Injectable()export class ContactService {  API_ENDPOINT = 'http://localhost:4201/api';  constructor(private http: HttpClient) {  }  getContacts(): Observable<Array<Contact>> {    const url = this.API_ENDPOINT + '/contacts';    return this.http.get<ContactsResponse>(url )      .pipe(map((data) => data.items));  }  getContact(id: string): Observable<Contact> {    const url = this.API_ENDPOINT + '/contacts/' + id;    const myParams = new HttpParams();    myParams.append('id', id);    return this.http.get<ContactResponse>(url)      .pipe(map((data) => data.item));  }}
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Contact} from './models/contact';
+import {Observable} from 'rxjs/Observable';
+
+interface ContactResponse {
+  item: Contact;
+}
+
+interface ContactsResponse {
+  items: Contact[];
+}
+
+@Injectable()
+export class ContactService {
+  API_ENDPOINT = 'http://localhost:4201/api';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getContacts(): Observable<Array<Contact>> {
+    const url = this.API_ENDPOINT + '/contacts';
+    return this.http.get<ContactsResponse>(`${this.API_ENDPOINT}/contacts/`)
+      .pipe(map((data) => data.items));
+  }
+
+  getContact(id: string): Observable<Contact> {
+    return this.http.get<ContactResponse>(`${this.API_ENDPOINT}/contacts/${id}`)
+      .pipe(map((data) => data.item));
+  }
+}
